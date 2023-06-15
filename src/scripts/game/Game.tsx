a607ae4f57266bf17d2a8cfb718b1e669b4eefe8
arbassic <arbassic@gmail.com>
2023-06-15 10:35:02 +0200

    refactor: initialState is no more needed

--- src/scripts/game/Game.tsx
+++ src/scripts/game/Game.tsx
@@ -10,13 +10,13 @@ import { logAs } from './systems/Logger';
 import { useGame } from './useGame';
 import { prepareStage } from './view/prepareStage';
 
-import type { GameDispatch, GameState } from './GameContext';
+import type { GameDispatch } from './GameContext';
 
 let gameInstance: Application;
 let gameWrapper: HTMLDivElement;
 
 let timeoutId: ReturnType<typeof setTimeout> | undefined;
-function createPixiInstance(canvas: HTMLCanvasElement, dispatch: GameDispatch, initialState: GameState) {
+function createPixiInstance(canvas: HTMLCanvasElement, dispatch: GameDispatch) {
   if (gameInstance) return;
 
   // React "Strict.Mode" double-instance mitigation
@@ -55,24 +55,21 @@ function createPixiInstance(canvas: HTMLCanvasElement, dispatch: GameDispatch, i
 }
 
 export const Game = (): JSX.Element => {
-  // we need to pass the game state's dispatch function down to Phaser
-  const { dispatch, state } = useGame();
+  // we need to pass the game state's dispatch function down to the Game
+  const { dispatch } = useGame();
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   // create phaser instance
   useEffect(() => {
     if (canvasRef.current) {
-      createPixiInstance(canvasRef.current, dispatch, state);
+      createPixiInstance(canvasRef.current, dispatch);
     }
 
     return () => {
       if (!gameInstance) return;
       gameInstance.destroy(true);
     };
-
-    // Do not recreate canvas if state changes (pass only initial state here)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [canvasRef, dispatch]);
 
   return (
